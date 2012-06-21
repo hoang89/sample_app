360893ddc0f928faea1f885e8953534241acd406
Nguyen Ngoc Hoang <nguyenngochoang89@gmail.com>
2012-06-21 09:57:46 +0700

    sua

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,8 +11,8 @@ subject{ page }
 
   describe "profile page" do
   # Code to make a user variable
-  # let(:user) { FactoryGirl.create(:user) }
-  let(:user) { User.find(44) }
+  let(:user) { FactoryGirl.create(:user) }
+  #let(:user) { User.find(44) }
   before { visit user_path(user) }
 
   it { should have_selector('h1', text: user.name) }
